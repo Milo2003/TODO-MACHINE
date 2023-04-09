d86efac98991d56c171ab723589186c5dd9135c3
Milo2003 <milonorenalo@gmail.com>
2023-04-08 22:20:40 -0500

    Terminamos el curso de intro a react, sigue hacer el deploy con github pages

--- src/EmptyTodos/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export function EmptyTodos() {
-    return (
-        <p>Crea TODOs</p>
-    )
-}
--- src/TodosError/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function TodosError({ error }) {
-    return <p>{error}</p>
-}
